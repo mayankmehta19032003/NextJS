ee2c94f0a1ecd2e0a77d51562ffcdd5f62491a58
Mayank Mehta <mayankmehta880@gmail.com>
2025-11-05 16:58:09 +0530

    completed form page

--- day2-Foodies/app/meals/[slug]/page.js
+++ day2-Foodies/app/meals/[slug]/page.js
@@ -1,9 +1,13 @@
 import styles from './page.module.css';
 import Image from 'next/image';
 import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
 
 const MealsDetailsPage = ({params}) => {
   const meal = getMeal(params.slug);
+  if(!meal){
+    notFound();
+  }
   meal.instructions = meal.instructions.replace(/\n/g,'<br/>');
   return (
     <>
