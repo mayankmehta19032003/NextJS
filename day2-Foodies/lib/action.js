1198da7c8e34c9658a2bb668baee062d6ee2c354
Mayank Mehta <mayankmehta880@gmail.com>
2025-11-06 22:26:30 +0530

    Add initial project setup with dependencies and image file
    - Added package.json with dependencies: slugify and xss
    - Created package-lock.json to lock dependency versions
    - Added an image file to the public/images directory

--- day2-Foodies/lib/action.js
+++ day2-Foodies/lib/action.js
@@ -1,5 +1,8 @@
 "use server";
 
+import { redirect } from "next/navigation";
+import { saveMeal } from "./meals";
+
 export async function shareMeal(formData){
 
     const meal = {
@@ -10,5 +13,7 @@ export async function shareMeal(formData){
       creator : formData.get('name'),
       creator_email : formData.get('email'),
     }
-    console.log(meal);
+    
+    await saveMeal(meal);
+    redirect('/meals');
   }
--- day2-Foodies/lib/meals.js
+++ day2-Foodies/lib/meals.js
@@ -1,4 +1,8 @@
+import fs from 'node:fs';
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
+import { error } from 'node:console';
 
 const db = sql('meals.db');
 
@@ -12,3 +16,28 @@ export async function getAllMeals() {
 export function getMeal(slug) {
   return db.prepare('SELECT * from meals WHERE slug = ?').get(slug);
 }
+
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split('.').pop();
+  const fileName = `${meal.slug}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer(); 
+
+  stream.write(Buffer.from(bufferedImage),(error) => {
+    if (error) {
+      throw new Error('Image upload failed');
+    }
+  });
+  
+  meal.image = `/images/${fileName}`;
+
+  db.prepare(`
+    INSERT INTO meals (title, summary, instructions, image, creator, creator_email, slug)
+    VALUES (@title, @summary, @instructions, @image, @creator, @creator_email, @slug)
+  `).run(meal);
+}
+
