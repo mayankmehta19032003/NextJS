24ca9ee87cbd7d31c7089f7294139ab5680be744
Mayank Mehta <mayankmehta880@gmail.com>
2025-11-20 19:22:39 +0530

    completed Project

--- day3/app/(marketing)/layout.js
+++ day3/app/(marketing)/layout.js
@@ -1,5 +1,4 @@
-import Header from "@/components/header";
-import "./globals.css";
+import "../globals.css";
 
 export const metadata = {
   title: "Next.js Page Routing & Rendering",
@@ -10,10 +9,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div id="page">
-          <Header />
+        
           {children}
-        </div>
+       
       </body>
     </html>
   );
